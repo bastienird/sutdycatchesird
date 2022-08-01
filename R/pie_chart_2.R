637a5d13f3a9b8873e6584df2ce11cfa503c4e54
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-08-01 10:20:44 +0200

    new one with color harmonization between dataset

--- R/pie_chart_2.R
+++ R/pie_chart_2.R
@@ -14,57 +14,67 @@
 #' @export
 #'
 #' @examples
-pie_chart_2 = function(dimension, unit = c("MT","MTNO","Tons","t"), first, second) {
+#'
+pie_chart_2 = function(dimension, first, second, topn = 5) {
+  library(dplyr)
+  if(any(first$unit == "MTNO")) first[first$unit == "MTNO", ]$unit <- "MT"
+  if(any(first$unit == "NOMT")) first[first$unit == "NOMT", ]$unit <- "NO"
+  if(any(second$unit == "MTNO")) second[second$unit == "MTNO", ]$unit <- "MT"
+  if(any(second$unit == "NOMT")) second[second$unit == "NOMT", ]$unit <- "NO"
+  first[is.na(first)] <- "NA"
+  second[is.na(second)] <- "NA"
+
   r <- deparse(substitute(dimension))
-  colnames <- enquo(dimension)
-  name1 <- enquo(first)
-  name2 <- enquo(second)
-  first <- first %>% filter(unit%in%unit)
-  second <- second %>% filter(unit%in%unit)
-  provisoire_i <-first %>%  group_by(!!colnames#, unit
-  )  %>% summarise(value = sum(value, na.rm = TRUE)) %>% #group_by(unit) %>%
-    arrange(desc(value)) %>%   mutate(id = row_number())%>%
-    mutate(class = as.factor(ifelse(id<4,!!colnames,"Others"))) %>%
-    group_by(class#, unit
-    ) %>%  summarise(value = sum(value, na.rm = TRUE))%>%ungroup()%>%
-    select( value, class,#unit
-    ) %>%  # group_by(unit) %>%
-    mutate(pourcentage = prop.table(value)*100)%>%
-    mutate(labels = paste0(pourcentage," ",  " % "))%>% arrange(desc(class))%>%
-    mutate(ypos_ligne = cumsum(pourcentage)- 0.5*pourcentage ) %>%
-    distinct()
+
+  colnames <- dplyr::enquo(dimension)
+
+  name1 <- dplyr::enquo(titre_1)
+  name2 <- dplyr::enquo(titre_2)
+  provisoire_i <-na.omit(first) %>%  dplyr::group_by(!!colnames, unit
+  )  %>% dplyr::summarise(value = sum(value, na.rm = TRUE)) %>% dplyr::group_by(unit) %>%
+    dplyr::arrange(desc(value)) %>%   dplyr::mutate(id = row_number())%>%
+    dplyr::mutate(class = as.factor(ifelse(id<topn  ,!!colnames,"Others"))) %>%
+    dplyr::group_by(class, unit
+    ) %>%  dplyr::summarise(value = sum(value, na.rm = TRUE))%>%dplyr::ungroup()%>%
+    dplyr::select( value, class,unit) %>%   dplyr::group_by(unit) %>%
+    dplyr::mutate(pourcentage = prop.table(value)*100)%>%
+    dplyr::mutate(labels = paste0(pourcentage," ",  " % "))%>% dplyr::arrange(desc(class))%>%
+    dplyr::mutate(ypos_ligne = cumsum(pourcentage)- 0.5*pourcentage ) %>%
+    dplyr::distinct() %>% dplyr::filter(!is.na(class))
 
 
 
 
-  provisoire_t <- second %>%  group_by(!!colnames#, unit
-  )%>% summarise(value = sum(value, na.rm = TRUE)) %>%# group_by(unit) %>%
-    arrange(desc(value)) %>%   mutate(id = row_number())%>%
-    mutate(class = as.factor(ifelse(id<4,!!colnames,"Others"))) %>%
-    group_by(class#, unit
-    ) %>%  summarise(value = sum(value, na.rm = TRUE))%>%ungroup()%>%
-    select( value, class,#unit
-    ) %>%#   group_by(unit) %>%
-    mutate(pourcentage = prop.table(value)*100)%>%
-    mutate(labels = paste0(pourcentage," ",  " % "))%>% arrange(desc(class))%>%
-    mutate(ypos_ligne = cumsum(pourcentage)- 0.5*pourcentage ) %>%
-    distinct()
-
-# -------------------------------------------------------------------------
-
-
-
+  provisoire_t <- na.omit(second) %>%  dplyr::group_by(!!colnames, unit)%>% dplyr::summarise(value = sum(value, na.rm = TRUE)) %>% dplyr::group_by(unit) %>%
+    dplyr::arrange(desc(value)) %>%   dplyr::mutate(id = row_number())%>%
+    dplyr::mutate(class = as.factor(ifelse(id<topn,!!colnames,"Others"))) %>%
+    dplyr::group_by(class, unit
+    ) %>%  dplyr::summarise(value = sum(value, na.rm = TRUE))%>%dplyr::ungroup()%>%
+    dplyr::select( value, class,unit
+    ) %>%   dplyr::group_by(unit) %>%
+    dplyr::mutate(pourcentage = prop.table(value)*100)%>%
+    dplyr::mutate(labels = paste0(pourcentage," ",  " % "))%>% dplyr::arrange(desc(class))%>%
+    dplyr::mutate(ypos_ligne = cumsum(pourcentage)- 0.5*pourcentage ) %>%
+    dplyr::distinct()%>% dplyr::filter(!is.na(class))
 
   set.seed(2) # For reproducibility of random color vector
+  # print(c(provisoire_i$class, provisoire_t$class))
+  # print(unique(unlist(as.character(c(provisoire_i$class, provisoire_t$class)))))
   number <- length(unique(unlist(as.character(c(provisoire_i$class, provisoire_t$class)))))
-  pal <- brewer.pal(number,"Set1")
+  # print(number)
+  pal <- brewer.pal(number,"Paired")
+  #   display.brewer.all(n=NULL, type="all", select=NULL, exact.n=TRUE,
+  # colorblindFriendly=TRUE)
+  # print(number)
 
   # myColors <- ColorRampPalette(brewer.pal(120,"Spectral"))
   # names(myColors) <- levels(dat$grp)
   # set.seed(2)
+  # print(pal)
   pal = setNames(pal, unique(unlist(as.character(c(provisoire_i$class, provisoire_t$class)))))
+  # print(pal)
 
-  ggplot_i <<- ggplot(provisoire_i) +
+  ggplot_i <<- ggplot(provisoire_i%>% dplyr::filter(!is.na(class))) +
     aes(
       x = "",
       fill = class,
@@ -74,14 +84,17 @@ pie_chart_2 = function(dimension, unit = c("MT","MTNO","Tons","t"), first, secon
     geom_bar(position = "fill") +
     scale_fill_hue(direction = 1) +
     scale_color_hue(direction = 1) +
-    theme_minimal()+ coord_polar("y", start=0)+ geom_text(first = (provisoire_i %>% mutate_if(is.numeric, round)), size = 3,
+    theme_minimal()+ coord_polar("y", start=0)+ geom_text(first = (provisoire_i %>% dplyr::filter(!is.na(class))%>%dplyr::mutate_if(is.numeric, round)), size = 3,
                                                           aes( x = 1 ,y = ypos_ligne/100, label = paste0(round(pourcentage),"%")), color = "black")+
     theme(axis.ticks.x = element_blank(),
           axis.text.x = element_blank())+
-    labs(x = "", y="")+ scale_fill_manual(values=pal)+ guides(fill=guide_legend(title=toupper(r)))
-  legend <- cowplot::get_legend(ggplot_i)
+    labs(x = "", y="")+ scale_fill_manual(values=pal)+ guides(fill=guide_legend(title=toupper(r)))+facet_wrap("unit")+
+    scale_fill_discrete(na.translate = F)
+  # print("ggplot_i")
+  legend <- cowplot::get_legend(ggplot_i+
+                                  scale_fill_discrete(na.translate = F))
 
-  ggplot_t <<- ggplot(provisoire_t) +
+  ggplot_t <<- ggplot(provisoire_t%>% dplyr::filter(!is.na(class))) +
     aes(
       x = "",
       fill = class,
@@ -91,11 +104,12 @@ pie_chart_2 = function(dimension, unit = c("MT","MTNO","Tons","t"), first, secon
     geom_bar(position = "fill") +
     scale_fill_hue(direction = 1) +
     scale_color_hue(direction = 1) +
-    theme_minimal()+ coord_polar("y", start=0)+ geom_text(first = (provisoire_t %>% mutate_if(is.numeric, round)), size = 3,
+    theme_minimal()+ coord_polar("y", start=0)+ geom_text(first = (provisoire_t%>% dplyr::filter(!is.na(class)) %>%dplyr::mutate_if(is.numeric, round)), size = 3,
                                                           aes( x = 1, y = ypos_ligne/100, label = paste0(round(pourcentage),"%")), color = "black")+
     theme(axis.ticks.x = element_blank(),
           axis.text.x = element_blank())+
-    labs(x = "", y="")+ scale_fill_manual(values=pal)+ theme(legend.position = "none")
+    labs(x = "", y="")+ scale_fill_manual(values=pal)+ theme(legend.position = "none")+facet_wrap("unit")+
+    scale_fill_discrete(na.translate = F)
 
   title <- ggdraw() +
     draw_label(
@@ -109,8 +123,8 @@ pie_chart_2 = function(dimension, unit = c("MT","MTNO","Tons","t"), first, secon
       # so title is aligned with left edge of first plot
       plot.margin = margin(0, 0, 0, 7)
     )
-  graph <<- plot_grid(ggplot_i+ theme(legend.position = "none"), ggplot_t,nrow = 2,labels = c( deparse(substitute(name1)),deparse(substitute(name2))),
-                      label_size = 10)
+  graph <<- plot_grid(ggplot_i+ theme(legend.position = "none"), ggplot_t, nrow = 2,labels = c( gsub('"','',gsub('~"','',deparse(substitute(name1)))),gsub('"','',gsub('~"','',deparse(substitute(name2))))),
+                      label_size = 10, vjust = 1.3, label_x = c(0,0), label_y = 1.025, axis = "l", align = "v")
 
   plot_grid(title, nrow=2,plot_grid(graph,legend,ncol = 2),
             # rel_heights values control vertical title margins
