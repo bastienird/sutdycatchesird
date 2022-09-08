fb83ebf115911f3adee1a576b4510562a7f78b89
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-09-08 17:56:05 +0200

    addin df return

--- R/pie_chart_2.R
+++ R/pie_chart_2.R
@@ -21,8 +21,9 @@
 #'
 #'
 pie_chart_2 =function (dimension, first, second = NULL, topn = 4, titre_1 = "first",
-                       titre_2 = "second", title_yes_no = TRUE)
+                       titre_2 = "second", title_yes_no = TRUE, dataframe = FALSE)
 {
+
   if (any(first$unit == "MTNO"))
     first[first$unit == "MTNO", ]$unit <- "MT"
   if (any(first$unit == "NOMT"))
@@ -74,6 +75,17 @@ pie_chart_2 =function (dimension, first, second = NULL, topn = 4, titre_1 = "fir
                       0.5 * pourcentage) %>% dplyr::distinct() %>%
       dplyr::filter(!is.na(class))
   }
+  if(!is.null(second)){
+    disappearing_stratas <- anti_join(provisoire_i %>% select(class), provisoire_t%>% select(class))%>% distinct()
+    appearing_stratas <- anti_join(provisoire_t%>% select(class), provisoire_i%>% select(class)) %>% distinct()
+    number_disappearing_stratas <- nrow(disappearing_stratas)
+    number_appearing_stratas <- nrow(appearing_stratas)
+    summary_apparition <- ggdraw()+draw_label(paste0("Number of appearing stratas : ", number_appearing_stratas), size = 10)
+    if(number_appearing_stratas!= 0)summary_apparition <- summary_apparition+draw_label(paste0(" \nThey are ", paste((appearing_stratas %>% select(class) %>% distinct())$class, sep = ";")), size = 10)
+    summary_apparition <- summary_apparition+draw_label(paste0(" \nNumber of disappearing stratas : ", number_disappearing_stratas), size = 10)
+    if(number_disappearing_stratas!= 0)summary_apparition <- summary_apparition+draw_label(paste0(" \nThey are ", paste((disappearing_stratas %>% select(class) %>% distinct())$class, sep = ";")), size = 10)
+  }
+
   set.seed(2)
   if (!(is.null(second))) {
     number <- length(unique(unlist(as.character(c(provisoire_i$class,
@@ -91,7 +103,7 @@ pie_chart_2 =function (dimension, first, second = NULL, topn = 4, titre_1 = "fir
     pal = setNames(pal, unique(unlist(as.character(c(provisoire_i$class)))))
   }
 
-  ggplot_i <<- ggplot(provisoire_i %>% dplyr::filter(!is.na(class))) +
+  ggplot_i <- ggplot(provisoire_i %>% dplyr::filter(!is.na(class))) +
     aes(x = "", fill = class, group = class, weight = pourcentage) +
     geom_bar(position = "fill") + scale_fill_hue(direction = 1) +
     scale_color_hue(direction = 1) + theme_minimal() + coord_polar("y",
@@ -128,33 +140,59 @@ pie_chart_2 =function (dimension, first, second = NULL, topn = 4, titre_1 = "fir
                                         r), fontface = "bold", x = 0, hjust = 0) + theme(plot.margin = margin(0,
                                                                                                               0, 0, 7))
   if (!is.null(second)) {
-    graph <<- plot_grid(ggplot_i + theme(legend.position = "none"),
-                        ggplot_t, nrow = 2, labels = c(gsub("\"", "", gsub("~\"",
-                                                                           "", deparse(substitute(name1)))), gsub("\"",
-                                                                                                                  "", gsub("~\"", "", deparse(substitute(name2))))),
-                        label_size = 10, vjust = 1.3, label_x = c(0, 0),
-                        label_y = 1.025, axis = "l", align = "v")
-    ploting_map <<- plot_grid(title, nrow = 2, plot_grid(graph,
-                                                         legend, ncol = 2), rel_heights = c(0.1, 1)) + theme(plot.background = element_rect(color = "black"))
+    graph <- plot_grid(ggplot_i + theme(legend.position = "none"),
+                       ggplot_t, nrow = 2, labels = c(gsub("\"", "", gsub("~\"",
+                                                                          "", deparse(substitute(name1)))), gsub("\"",
+                                                                                                                 "", gsub("~\"", "", deparse(substitute(name2))))),
+                       label_size = 10, vjust = 1.3, label_x = c(0, 0),
+                       label_y = 1.025, axis = "l", align = "v")
+    ploting_map <- plot_grid(title, nrow = 2, plot_grid(graph,
+                                                        legend, ncol = 2), rel_heights = c(0.1, 1)) + theme(plot.background = element_rect(color = "black"))
     if (sum(!(round(provisoire_i$pourcentage) == round(provisoire_t$pourcentage))) ==
         0) {
       title <- ggdraw() + draw_label(paste0("Distribution in value for the dimension : ",
-                                            r, " \n (same to the nearest rounding for both datasets)"),
-                                     fontface = "bold", x = 0, hjust = 0) + theme(plot.margin = margin(0,
-                                                                                                       0, 0, 7))
-      ploting_map <- plot_grid(title, ggplot_i, nrow = 2,
-                               rel_heights = c(0.5, 1)) + theme(plot.background = element_rect(color = "black"))
+                                            r, "\n(same distribution to the nearest rounding for both datasets : ",
+                                            gsub("\"", "", gsub("~\"",
+                                                                "", deparse(substitute(name1))))," and ", gsub("\"",
+                                                                                                               "", gsub("~\"", "", deparse(substitute(name2)))),")"),
+                                     fontface = "bold", x = 0, hjust = 0, vjust = 0.5, size = 13) + theme(plot.margin = margin(0,
+                                                                                                                               0, 0, 7))
+      graph <- ggplot_i+ theme(legend.position = "none")
     }
   }
   else {
-    graph <<- plot_grid(ggplot_i + theme(legend.position = "none"),
-                        nrow = 1, labels = c(gsub("\"", "", gsub("~\"",
-                                                                 "", deparse(substitute(name1))))), label_size = 10,
-                        vjust = 1.3, label_x = c(0, 0), label_y = 0.8, axis = "l",
-                        align = "v")
-    ploting_map <<- plot_grid(title, nrow = 2, plot_grid(graph,
-                                                         legend, ncol = 2), rel_heights = c(0.1, 1)) + theme(plot.background = element_rect(color = "black"))
+    graph <- plot_grid(ggplot_i + theme(legend.position = "none"),
+                       nrow = 1, labels = c(gsub("\"", "", gsub("~\"",
+                                                                "", deparse(substitute(name1))))), label_size = 10,
+                       vjust = 1.3, label_x = c(0, 0), label_y = 0.8, axis = "l",
+                       align = "v")
+
   }
-  if(!title_yes_no) {ploting_map <- ploting_map+title=element_blank()}
-  ploting_map
+  if(!title_yes_no){
+    if (sum(!(round(provisoire_i$pourcentage) == round(provisoire_t$pourcentage))) ==
+        0) {      title <- ggdraw() + draw_label(paste0("(same distribution to the nearest rounding for both datasets :\n",
+                                                        gsub("\"", "", gsub("~\"",
+                                                                            "", deparse(substitute(name1))))," and ", gsub("\"",
+                                                                                                                           "", gsub("~\"", "", deparse(substitute(name2)))),")"),
+                                                 fontface = "bold", x = 0, hjust = 0, vjust = 0.5, size = 13) + theme(plot.margin = margin(0,
+                                                                                                                                           0, 0, 7))}
+    else {
+      title <- ggdraw() + draw_label(" \n ")
+    }
+  }
+
+  ploting_map <- plot_grid(title, nrow = 2, plot_grid(graph,
+                                                      legend, ncol = 2), rel_heights = c(0.1, 1)) + theme(plot.background = element_rect(color = "black"))
+  if(exists("summary_apparition") & dataframe){
+    df <- data.frame(' '= c("Stratas appearing","Stratas disappearing"),
+                     'Number' = c(number_appearing_stratas,number_disappearing_stratas ),
+  'Detail' = c(toString(paste((appearing_stratas %>% select(class) %>% distinct())$class, sep = ";")),
+     toString(paste((disappearing_stratas %>% select(class) %>% distinct())$class, sep = ";"))),check.names = FALSE,fix.empty.names = FALSE)
+    if(number_disappearing_stratas == 0 & number_appearing_stratas ==0){df <- df %>% select(-Detail)}
+    list_df_plot <- list(plot = ploting_map, df =df)
+  return(list_df_plot)
+} else{return(ploting_map)}
 }
+
+
+
